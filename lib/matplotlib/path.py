245e7107830da3426487a8888f68feb326aaf76d
anTon <138380708+r3kste@users.noreply.github.com>
2024-10-22 11:26:14 +0530

    minor refactor

--- lib/matplotlib/path.py
+++ lib/matplotlib/path.py
@@ -1037,10 +1037,10 @@ class Path:
         return (get_path(hatchpattern, density)
                 if hatchpattern is not None else None)
 
-    def hatchstyles(hatchstyles, hatch_scale=1):
+    def hatchstyles(hatchstyles, hatch_buffer_scale=1.0):
         vertices, codes = np.empty((0, 2)), np.empty(0, Path.code_type)
         for hatchstyle in hatchstyles:
-            verts, cods = hatchstyle.get_vertices_and_codes(hatch_scale)
+            verts, cods = hatchstyle.get_vertices_and_codes(hatch_buffer_scale)
             vertices = np.concatenate([vertices, verts])
             codes = np.concatenate([codes, cods])
         return Path(vertices, codes)
